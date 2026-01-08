ec40fc1b5d89307063c6a94bf85728a7c9b9624f
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-01-08 10:58:23 +0530

    Initialized Contact Management App

---
+++ ContactApp/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="w-full h-screen bg-black text-white text-center text-4xl">Contact App React Project</h1>
+    </>
+  )
+}
+
+export default App
